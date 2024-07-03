a94ea260238f25d3d7f1aa81e7abc66dea79e2b8
shajigholam <shajigholam@myseneca.ca>
2024-07-02 23:35:10 -0400

    Adding the navigation

--- App.js
+++ App.js
@@ -1,12 +1,68 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import * as Font from "expo-font";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import ChatListScreen from "./screens/ChatListScreen";
+
+SplashScreen.preventAutoHideAsync();
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  // load font
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          black: require("./assets/fonts/Roboto-Black.ttf"),
+          blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          bold: require("./assets/fonts/Roboto-Bold.ttf"),
+          boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          italic: require("./assets/fonts/Roboto-Italic.ttf"),
+          light: require("./assets/fonts/Roboto-Light.ttf"),
+          lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+          medium: require("./assets/fonts/Roboto-Medium.ttf"),
+          mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          regular: require("./assets/fonts/Roboto-Regular.ttf"),
+          thin: require("./assets/fonts/Roboto-Thin.ttf"),
+          thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setAppIsLoaded(true);
+      }
+    };
+
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
+      <SafeAreaView>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={ChatListScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -16,5 +72,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  label: {
+    color: "black",
+    fontSize: 18,
+    fontFamily: "regular",
   },
 });
---
+++ screens/ChatListScreen.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { View, Text } from "react-native";
+
+const ChatListScreen = (props) => {
+  return (
+    <View>
+      <Text>Chat list screen</Text>
+    </View>
+  );
+};
+
+export default ChatListScreen;
