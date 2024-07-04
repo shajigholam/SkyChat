86e17148f3c0f8d936df994e0070830e4728031a
shajigholam <shajigholam@myseneca.ca>
2024-07-04 16:45:55 -0400

    Evaluating the whole form's validity

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Feather, FontAwesome } from "@expo/vector-icons";
 
 import Input from "../components/Input";
 import SubmitButton from "../components/SubmitButton";
 import { validateInput } from "../utils/actions/formActions";
 
+const reducer = (state, action) => {
+  // const validationResult = action.validationResult;
+  const { validationResult, inputId } = action;
+
+  const updatedValidities = {
+    ...state.inputValidities,
+    [inputId]: validationResult,
+  };
+
+  let updatedFormIsValid = true;
+
+  for (const key in updatedValidities) {
+    if (updatedValidities[key] !== undefined) {
+      updatedFormIsValid = false;
+      break;
+    }
+  }
+
+  return {
+    inputValidities: updatedValidities,
+    formIsValid: updatedFormIsValid,
+  };
+};
+
+const initialState = {
+  inputValidities: {
+    firstName: false,
+    lastName: false,
+    email: false,
+    password: false,
+  },
+  formIsValid: false,
+};
 const SignUpForm = (props) => {
+  const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
   const inputChangeHandler = (inputId, inputValue) => {
-    console.log(validateInput(inputId, inputValue));
+    const result = validateInput(inputId, inputValue);
+    dispatchFormState({ inputId: inputId, validationResult: result });
   };
 
   return (
@@ -48,6 +84,7 @@ const SignUpForm = (props) => {
         title="Sign up"
         onPress={() => console.log("pressed")}
         style={{ marginTop: 20 }}
+        disabled={!formState.formIsValid}
       />
     </>
   );
