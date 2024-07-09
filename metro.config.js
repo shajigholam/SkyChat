1506e6810c38d6c817873b7df97f365427037ef9
shajigholam <shajigholam@myseneca.ca>
2024-07-08 23:47:45 -0400

    fixed the bug in metro config file

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,15 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+// const {getDefaultConfig} = require("@expo/metro-config");
 
+// const defaultConfig = getDefaultConfig(__dirname);
+
+// defaultConfig.resolver.assetExts.push(
+//   // Adds support for `.db` files for SQLite databases
+//   "cjs"
+// );
+
+// module.exports = defaultConfig;
+
+const {getDefaultConfig} = require("expo/metro-config");
 const defaultConfig = getDefaultConfig(__dirname);
-
-defaultConfig.resolver.assetExts.push(
-  // Adds support for `.db` files for SQLite databases
-  "cjs"
-);
-
+defaultConfig.resolver.sourceExts.push("cjs");
 module.exports = defaultConfig;
