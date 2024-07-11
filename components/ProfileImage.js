5a3a465e0f7e714fcddde36a645887999d002645
shajigholam <shajigholam@myseneca.ca>
2024-07-11 10:32:31 -0400

    Updating the page to show the image

--- components/ProfileImage.js
+++ components/ProfileImage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {FontAwesome} from "@expo/vector-icons";
 
@@ -7,13 +7,28 @@ import colors from "../constants/colors";
 import {launchImagePicker} from "../utils/imagePickerHelper";
 
 const ProfileImage = props => {
-  const pickImage = () => {
-    launchImagePicker();
+  const source = props.uri ? {uri: props.uri} : userImage;
+
+  const [image, setImage] = useState(source);
+
+  const pickImage = async () => {
+    try {
+      const tempUri = await launchImagePicker();
+
+      if (!tempUri) return;
+
+      // upload the image
+
+      // set the image
+      setImage({uri: tempUri});
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <TouchableOpacity onPress={pickImage}>
       <Image
-        source={userImage}
+        source={image}
         style={{...styles.image, ...{width: props.size, height: props.size}}}
       />
       <View style={styles.editIconContainer}>
--- utils/imagePickerHelper.js
+++ utils/imagePickerHelper.js
@@ -1,8 +1,40 @@
 import * as ImagePicker from "expo-image-picker";
 import {Platform} from "react-native";
+import {getFirebaseApp} from "./firebaseHelper";
 
 export const launchImagePicker = async () => {
   await checkMediaPermissions();
+
+  const result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [1, 1],
+    quality: 1,
+  });
+
+  if (!result.canceled) {
+    // console.log(result.uri);
+    return result.uri;
+  }
+};
+
+export const uploadImageAync = async uri => {
+  const app = getFirebaseApp();
+
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+
+    xhr.onerror = function (e) {
+      console.log(e);
+      reject(new TypeError("Network request failed"));
+    };
+    xhr.responseType = "blob";
+    xhr.open("GET", uri, true);
+    xhr.send();
+  });
 };
 
 const checkMediaPermissions = async () => {
