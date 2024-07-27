81d16cf2afd41180fce620ec7cd1b905ec489529
shajigholam <shajigholam@myseneca.ca>
2024-07-27 00:09:40 -0400

    Stylying the reply section

--- components/ReplyTo.js
+++ components/ReplyTo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
+import colors from "../constants/colors";
 
 const ReplyTo = props => {
   const {text, user, onCancel} = props;
@@ -11,16 +12,30 @@ const ReplyTo = props => {
         <Text numberOfLines={1} style={styles.name}>
           {name}
         </Text>
-        <Text numberOfLines={1} style={{color: "white"}}>
-          {text}
-        </Text>
+        <Text numberOfLines={1}>{text}</Text>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  name: {color: "white"},
+  container: {
+    backgroundColor: colors.extraLightGrey,
+    padding: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    borderLeftColor: colors.blue,
+    borderLeftWidth: 4,
+  },
+  textContainer: {
+    flex: 1,
+    marginRight: 5,
+  },
+  name: {
+    color: colors.blue,
+    fontFamily: "medium",
+    letterSpacing: 0.3,
+  },
 });
 
 export default ReplyTo;
