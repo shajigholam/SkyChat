66f025e9b1cd06fa7ec60173f53fc95d78282d5e
shajigholam <shajigholam@myseneca.ca>
2024-08-08 16:15:50 -0400

    Created the form handler for updating the chat name

--- screens/ChatSettingScreen.js
+++ screens/ChatSettingScreen.js
@@ -1,14 +1,64 @@
-import react from "react";
+import react, {useCallback, useReducer, useState} from "react";
 import {View, Text, StyleSheet, ScrollView} from "react-native";
 import {useSelector} from "react-redux";
 import PageContainer from "../components/PageContainer";
 import PageTitle from "../components/PageTitle";
 import ProfileImage from "../components/ProfileImage";
+import Input from "../components/Input";
+import {reducer} from "../utils/reducers/formReducer";
+import {validateLength} from "../utils/validationConstraints";
+import {updateChatData} from "../utils/actions/chatActions";
 
 const ChatSettingScreen = props => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
   const chatId = props.route.params.chatId;
   const chatData = useSelector(state => state.chats.chatsData[chatId]);
   const userData = useSelector(state => state.auth.userData);
+
+  const initialState = {
+    inputValues: {chatName: chatData.chatName},
+    inputValidities: {chatName: undefined},
+    formIsValid: false,
+  };
+
+  const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
+  const inputChangeHandler = useCallback(
+    (inputId, inputValue) => {
+      const result = validateLength(inputId, inputValue);
+      dispatchFormState({
+        inputId: inputId,
+        validationResult: result,
+        inputValue: inputValue,
+      });
+    },
+    [dispatchFormState]
+  );
+
+  const saveHandler = useCallback(async () => {
+    const updatedValues = formState.inputValues;
+    try {
+      setIsLoading(true);
+      await updateChatData(chatId, userData.userId, updatedValues);
+
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [formState, dispatch]);
+
+  const hasChanges = () => {
+    const currentValues = formState.inputValues;
+
+    return currentValues.chatName != chatData.chatName;
+  };
 
   return (
     <PageContainer>
@@ -20,8 +70,16 @@ const ChatSettingScreen = props => {
           chatId={chatId}
           userId={userData.userId}
           uri={chatData.chatImage}
+          onInputChanged={inputChangeHandler}
         />
-        <Text>{chatData.chatName}</Text>
+
+        <Input
+          id="chatName"
+          label="Chat name"
+          autoCapitalize="none"
+          initialValue={chatData.chatName}
+          allowEmpty={false}
+        />
       </ScrollView>
     </PageContainer>
   );
